cd6a28781fd4106aa9eaad9707d2111ceaf2d53f
Dylan Montagu <dmontagu@dm.lan>
2023-03-07 19:00:34 -0500

    - Create tests for generate method

--- src/test/java/com/dylanmontagu/primenumbergenerator/SquareRootIterativePrimeNumberGeneratorTest.java
+++ src/test/java/com/dylanmontagu/primenumbergenerator/SquareRootIterativePrimeNumberGeneratorTest.java
@@ -24,15 +24,29 @@ public class SquareRootIterativePrimeNumberGeneratorTest {
 		});
 	}
 	
-	
 	@Test
 	public void testIsPrimeOnNonPrimeNumbers() {
-		
 		List<Integer> nonPrimeNumbers = Arrays.asList(6, 1, -2, 7010, -1000, 0);
 		nonPrimeNumbers.forEach(nonPrimeNumber -> {
 			assertFalse(generator.isPrime(nonPrimeNumber));
 		});
-
 	}
 
+	@Test
+	public void testGenerateWithNumericallyIncreasingArguments() {
+		List<Integer> primeNumberResults = Arrays.asList(7901, 7907, 7919);
+		assertEquals(primeNumberResults, generator.generate(7900, 7920));
+
+		primeNumberResults = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19);
+		assertEquals(primeNumberResults, generator.generate(-10, 20));
+	}
+
+	@Test
+	public void testGenerateWithNumericallyDecreasingArguments() {
+		List<Integer> primeNumberResults = Arrays.asList(7901, 7907, 7919);
+		assertEquals(primeNumberResults, generator.generate(7920, 7900));
+
+		primeNumberResults = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19);
+		assertEquals(primeNumberResults, generator.generate(20, -10));
+	}
 }
